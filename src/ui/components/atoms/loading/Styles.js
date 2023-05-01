2aeb0690a31dbc880874a11c5a8d9df779e81d23
thiago5171 <tgazaroli@gmail.com>
2023-05-01 00:40:25 -0300

    Add loading component

---
+++ src/ui/components/atoms/loading/Styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#08244F",
+  },
+  loadingText: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    color: "#fff",
+  },
+  spinner: {
+    marginBottom: 50,
+    alignItems: "center",
+  },
+});
+
+export default styles;
---
+++ src/ui/components/atoms/loading/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text } from "react-native";
+import * as Animatable from "react-native-animatable";
+import { Feather } from "@expo/vector-icons";
+import styles from "./Styles";
+
+const Loading = () => {
+  return (
+    <View style={styles.container}>
+      <Animatable.View
+        animation="rotate"
+        easing="linear"
+        iterationCount="infinite"
+        style={styles.spinner}
+      >
+        <Feather name="sun" size={50} color="orange" />
+      </Animatable.View>
+      <Text style={styles.loadingText}>Carregando...</Text>
+    </View>
+  );
+};
+
+export default Loading;
